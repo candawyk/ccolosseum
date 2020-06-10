0eb18b9630727b0feb18cfae7b2e9e963adf14fc
Brandon Withington <withingb@oregonstate.edu>
2020-06-09 20:14:07 -0700

    Revert "Merge branch 'master' of https://github.com/winghenge/crittercolosseum"
    This reverts commit adfc6f4b03d0561bdfc8682c6b2f7a19e394117d, reversing
    changes made to 04ada61e49a6dc32cb602c321d1671f59abf3492.

--- startBattle.js
+++ startBattle.js
@@ -11,7 +11,7 @@ module.exports = function(){
     router.post('/', function(req,res){
       var context = {};
       var mysql = req.app.get('mysql');
-      var sql = "INSERT INTO Battle (Quantity_one, Quantity_two, Size_one, Size_two, Votes_one, Votes_two) VALUES(?,?,?,?,0,0)";
+      var sql = "INSERT INTO Battle (Quantity_one, Quantity_two, Size_one, Size_two) VALUES(?,?,?,?)";
       var nsql = "INSERT INTO Critter (Species) VALUES(?)";
       var tpi = "INSERT INTO Takes_part_in (critter_one, critter_two, battle) VALUES(?,?,?)";
 
@@ -93,9 +93,9 @@ module.exports = function(){
         	           res.end();
 		             }
 		               CTID = results.insertId;
-		                // console.log("++" + CTID);
+		                 console.log("++" + CTID);
                      var tpi_in = [COID, CTID, BID];
-                     //console.log([COID, CTID, BID]);
+                     console.log([COID, CTID, BID]);
                      sql = mysql.pool.query(tpi, tpi_in, function(error, results, fields){
       	                if (error){
                           res.write(JSON.stringify(error));
