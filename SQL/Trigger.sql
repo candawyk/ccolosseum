9c37a14fe2458ebc4369c664bb4094229f868fb6
Travis Seevers <seeverst@flip2.engr.oregonstate.edu>
2020-06-10 15:01:29 -0700

    final

--- SQL/Trigger.sql
+++ SQL/Trigger.sql
@@ -1,15 +1,18 @@
-delimiter $$
-CREATE TRIGGER `test_2`
-AFTER INSERT
-oN `Battle` FOR EACH row
+-- Trigger for: Votes_for
+-- AFTER INSERT
+-- Increments votes_one or votes_two depending on liked value (0 or 1)
 
 BEGIN
+	IF (New.battle IS NOT NULL) AND New.liked = 0 THEN
+    	UPDATE Battle
+        SET votes_one = votes_one + 1
+        WHERE New.battle = Battle.battle_id;
+    END IF;
 
-DECLARE n_id int(11);
-SELECT battle INTO lb from Votes_for ORDER BY id DESC LIMIT 1;
+ 	IF (New.battle IS NOT NULL) AND New.liked = 1 THEN
+    	UPDATE Battle
+        SET votes_two = votes_two + 1
+        WHERE New.battle = Battle.battle_id;
+    END IF;
 
-
-
-
-END$$
-DELIMITER ;
+END
