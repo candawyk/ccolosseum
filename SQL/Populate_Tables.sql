034db2e8228ecdda0d483a7056e397a0315743c5
Travis Seevers <seeverst@10-197-33-254.sds.oregonstate.edu>
2020-06-10 14:18:38 -0700

    more DQL edits, some pics were broken

--- SQL/Populate_Tables.sql
+++ SQL/Populate_Tables.sql
@@ -1,15 +1,15 @@
 --insert data into the User table as if they were creating their account for the first time, bio will be added later if they desire
-INSERT INTO Usr (screen_name, email, password) VALUES
-("James", "james@google.net", "password123"),
-("Wilbur", "willy@bur.tv", "123abc"),
-("Chap", "man@wo.com", "foobar"),
-("Man", "manny@man.man", "verymanly"),
-("winghenge", "wing@henge.com", "abcdefg"),
-("fred", "fred@scooby.snacks", "jinkies"),
-("Mcdonald", "whopper@fast.food", "supersizeme"),
-("King", "burger@sauce.saucy", "whyevenbother"),
-("Hamantha", "ham@for.face", "hammybone"),
-("Shaggy", "Need.food@belly.now", "yumyum");
+INSERT INTO Usr (screen_name, email, password, bio) VALUES
+("James", "james@google.net", "password123", "Hello"),
+("Wilbur", "willy@bur.tv", "123abc", "Howdy"),
+("Chap", "man@wo.com", "foobar", "Im a user!"),
+("Man", "manny@man.man", "verymanly", "PotatoesAreCritterrsToo"),
+("winghenge", "wing@henge.com", "abcdefg", "I beleive i can fly"),
+("fred", "fred@scooby.snacks", "jinkies", "Its just a mask!"),
+("Mcdonald", "whopper@fast.food", "supersizeme", "super size me"),
+("King", "burger@sauce.saucy", "whyevenbother", "jumbo fries"),
+("Hamantha", "ham@for.face", "hammybone", "i have a ham for a face"),
+("Shaggy", "Need.food@belly.now", "yumyum", "I never lose a battle with my critters!");
 
 --Create critters
 INSERT INTO Critter (species) VALUES
@@ -27,21 +27,21 @@ INSERT INTO Critter (species) VALUES
 --Create an avatar and link it to either a creature or an user
 INSERT INTO Avatar (image_location, description ) VALUES
 ("http://i.huffpost.com/gen/781204/thumbs/o-CUTE-ANIMALS-PHOTOS-facebook.jpg", "picture: 1"),
-("https://i.ytimg.com/vi/J0EUxEkm4kc/maxresdefault.jpg", "picture: 2"),
-("http://i.ytimg.com/vi/u7RUz5MiqGs/maxresdefault.jpg", "picture: 3"),
-("https://i.ytimg.com/vi/SzvxLUPw7Bc/maxresdefault.jpg", "picture: 4"),
-("https://i.ytimg.com/vi/7TQYnZ9He_Q/maxresdefault.jpg", "picture: 5"),
+("http://i.huffpost.com/gen/781204/thumbs/o-CUTE-ANIMALS-PHOTOS-facebook.jpg", "picture: 2"),
+("http://i.huffpost.com/gen/781204/thumbs/o-CUTE-ANIMALS-PHOTOS-facebook.jpg", "picture: 3"),
+("http://i.huffpost.com/gen/781204/thumbs/o-CUTE-ANIMALS-PHOTOS-facebook.jpg", "picture: 4"),
+("http://i.huffpost.com/gen/781204/thumbs/o-CUTE-ANIMALS-PHOTOS-facebook.jpg", "picture: 5"),
 ("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.wired.com%2Fwp-content%2Fuploads%2F2015%2F04%2F85120553.jpg&f=1&nofb=1", "picture: 6"),
 ("https://wallpapercave.com/wp/hbEVhfX.jpg", "picture: 7"),
-("https://i.ytimg.com/vi/IhzRgkv1tJc/maxresdefault.jpg", "picture: 8"),
+("http://i.huffpost.com/gen/781204/thumbs/o-CUTE-ANIMALS-PHOTOS-facebook.jpg", "picture: 8"),
 ("http://2.bp.blogspot.com/_eIwMvvvkFQk/TTFbMEUfo0I/AAAAAAAAADw/ADqNO-VDiO0/s1600/cute+animals+%25280%2529.jpg", "picture: 9"),
-("https://i.ytimg.com/vi/emqeezLPK7I/maxresdefault.jpg", "picture: 10"),
+("http://i.huffpost.com/gen/781204/thumbs/o-CUTE-ANIMALS-PHOTOS-facebook.jpg", "picture: 10"),
 ("https://www.ucdavis.edu/sites/default/files/styles/panopoly_image_full/public/home-site/blogs/one-health/blog-posts/2018/cow-field-one-health-uc-davis.jpg?itok=lrz5mpyq", "picture: 11"),
 ("https://www.pbs.org/wgbh/nova/media/original_images/horse-smile_2048x1152.jpg", "picture: 12"),
-("https://i.ytimg.com/vi/LduRCTRVM8A/maxresdefault.jpg", "picture: 13"),
+("http://4.bp.blogspot.com/-28JWho2EmXI/UDd98XboSuI/AAAAAAAAESg/1QnuEXUp3oM/s1600/Yellow-Duck-4.jpg", "picture: 13"),
 ("https://animals.sandiegozoo.org/sites/default/files/inline-images/llama.jpg", "picture: 14"),
 ("https://www.rover.com/blog/wp-content/uploads/2018/12/dog-sneeze-1-1024x945.jpg", "picture: 15"),
-("http://i.ytimg.com/vi/5VBriRtl_aM/maxresdefault.jpg", "picture: 16"),
+("https://www.readersdigest.ca/wp-content/uploads/sites/14/2016/05/cats-lick-themselves-to-clean-injuries.jpg", "picture: 16"),
 ("https://portal.andina.pe/EDPfotografia3/Thumbnail/2017/08/01/000439616W.jpg", "picture: 17"),
 ("https://upload.wikimedia.org/wikipedia/commons/6/6d/Metallic_shield_bug444.jpg", "picture: 18"),
 ("https://www.nydailynews.com/resizer/CEEJYohg9VZAqlqB4WEg0wTz4LA=/1200x0/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/TAU2JU73A5HNVJO7IPO5J3XJOU.jpg", "picture: 19"),
@@ -51,23 +51,23 @@ INSERT INTO User_has_a (usr, av_id) VALUES
 ((SELECT User_id FROM Usr WHERE screen_name="James"),
 (SELECT av_id FROM Avatar WHERE image_location="http://i.huffpost.com/gen/781204/thumbs/o-CUTE-ANIMALS-PHOTOS-facebook.jpg")),
 ((SELECT User_id FROM Usr WHERE screen_name="Wilbur"),
-(SELECT av_id FROM Avatar WHERE image_location="https://i.ytimg.com/vi/J0EUxEkm4kc/maxresdefault.jpg")),
+(SELECT av_id FROM Avatar WHERE image_location="http://i.huffpost.com/gen/781204/thumbs/o-CUTE-ANIMALS-PHOTOS-facebook.jpg")),
 ((SELECT User_id FROM Usr WHERE screen_name="Chap"),
-(SELECT av_id FROM Avatar WHERE image_location="http://i.ytimg.com/vi/u7RUz5MiqGs/maxresdefault.jpg")),
+(SELECT av_id FROM Avatar WHERE image_location="http://i.huffpost.com/gen/781204/thumbs/o-CUTE-ANIMALS-PHOTOS-facebook.jpg")),
 ((SELECT User_id FROM Usr WHERE screen_name="Man"),
-(SELECT av_id FROM Avatar WHERE image_location="https://i.ytimg.com/vi/SzvxLUPw7Bc/maxresdefault.jpg")),
+(SELECT av_id FROM Avatar WHERE image_location="http://i.huffpost.com/gen/781204/thumbs/o-CUTE-ANIMALS-PHOTOS-facebook.jpg")),
 ((SELECT User_id FROM Usr WHERE screen_name="winghenge"),
-(SELECT av_id FROM Avatar WHERE image_location="https://i.ytimg.com/vi/7TQYnZ9He_Q/maxresdefault.jpg")),
+(SELECT av_id FROM Avatar WHERE image_location="http://i.huffpost.com/gen/781204/thumbs/o-CUTE-ANIMALS-PHOTOS-facebook.jpg")),
 ((SELECT User_id FROM Usr WHERE screen_name="Fred"),
 (SELECT av_id FROM Avatar WHERE image_location="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.wired.com%2Fwp-content%2Fuploads%2F2015%2F04%2F85120553.jpg&f=1&nofb=1")),
 ((SELECT User_id FROM Usr WHERE screen_name="Mcdonald"),
 (SELECT av_id FROM Avatar WHERE image_location="https://wallpapercave.com/wp/hbEVhfX.jpg")),
 ((SELECT User_id FROM Usr WHERE screen_name="King"),
-(SELECT av_id FROM Avatar WHERE image_location="https://i.ytimg.com/vi/IhzRgkv1tJc/maxresdefault.jpg")),
+(SELECT av_id FROM Avatar WHERE image_location="http://i.huffpost.com/gen/781204/thumbs/o-CUTE-ANIMALS-PHOTOS-facebook.jpg")),
 ((SELECT User_id FROM Usr WHERE screen_name="Hamantha"),
 (SELECT av_id FROM Avatar WHERE image_location="http://2.bp.blogspot.com/_eIwMvvvkFQk/TTFbMEUfo0I/AAAAAAAAADw/ADqNO-VDiO0/s1600/cute+animals+%25280%2529.jpg")),
 ((SELECT User_id FROM Usr WHERE screen_name="Shaggy"),
-(SELECT av_id FROM Avatar WHERE image_location="https://i.ytimg.com/vi/emqeezLPK7I/maxresdefault.jpg"));
+(SELECT av_id FROM Avatar WHERE image_location="http://i.huffpost.com/gen/781204/thumbs/o-CUTE-ANIMALS-PHOTOS-facebook.jpg"));
 
 INSERT INTO Critter_has_a (critter, av_id) VALUES
 ((SELECT critter_id FROM Critter WHERE species="Cow"),
@@ -75,13 +75,13 @@ INSERT INTO Critter_has_a (critter, av_id) VALUES
 ((SELECT critter_id FROM Critter WHERE species="Horse"),
 (SELECT av_id FROM Avatar WHERE image_location="https://www.pbs.org/wgbh/nova/media/original_images/horse-smile_2048x1152.jpg")),
 ((SELECT critter_id FROM Critter WHERE species="Duck"),
-(SELECT av_id FROM Avatar WHERE image_location="https://i.ytimg.com/vi/LduRCTRVM8A/maxresdefault.jpg")),
+(SELECT av_id FROM Avatar WHERE image_location="http://4.bp.blogspot.com/-28JWho2EmXI/UDd98XboSuI/AAAAAAAAESg/1QnuEXUp3oM/s1600/Yellow-Duck-4.jpg")),
 ((SELECT critter_id FROM Critter WHERE species="Llama"),
 (SELECT av_id FROM Avatar WHERE image_location="https://animals.sandiegozoo.org/sites/default/files/inline-images/llama.jpg")),
 ((SELECT critter_id FROM Critter WHERE species="Dog"),
 (SELECT av_id FROM Avatar WHERE image_location="https://www.rover.com/blog/wp-content/uploads/2018/12/dog-sneeze-1-1024x945.jpg")),
 ((SELECT critter_id FROM Critter WHERE species="Cat"),
-(SELECT av_id FROM Avatar WHERE image_location="http://i.ytimg.com/vi/5VBriRtl_aM/maxresdefault.jpg")),
+(SELECT av_id FROM Avatar WHERE image_location="https://www.readersdigest.ca/wp-content/uploads/sites/14/2016/05/cats-lick-themselves-to-clean-injuries.jpg")),
 ((SELECT critter_id FROM Critter WHERE species="Alpaca"),
 (SELECT av_id FROM Avatar WHERE image_location="https://portal.andina.pe/EDPfotografia3/Thumbnail/2017/08/01/000439616W.jpg")),
 ((SELECT critter_id FROM Critter WHERE species="Bug"),
@@ -173,6 +173,3 @@ INSERT INTO Comments (comment_text, likes, dislikes) VALUES
 
 INSERT INTO Comments (comment_text, likes, dislikes) VALUES
 ("WordsWordsWords", 0, 0);
-
-
-
